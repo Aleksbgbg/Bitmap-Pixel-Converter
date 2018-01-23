1e77ffca9d350599a5be26cb38380a4ce661a0cc
Aleksbgbg <kozichka01@abv.bg>
2018-01-23 22:02:44 +0000

    Create Surface from bitmap stream.

--- Bitmap Pixel Converter/Main.cpp
+++ Bitmap Pixel Converter/Main.cpp
@@ -1,4 +1,33 @@
+#include "Surface.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+std::ifstream get_bitmap_stream()
+{
+	while (true)
+	{
+		std::cout << "Enter bitmap path: ";
+
+		std::string bitmapPath;
+
+		std::getline(std::cin, bitmapPath);
+
+		std::ifstream bitmapFile{ bitmapPath, std::ios::binary };
+
+		if (bitmapFile)
+		{
+			return bitmapFile;
+		}
+
+		std::cout << "File is not good.\n\n";
+	}
+}
+
 int main()
 {
+	const Surface image{ get_bitmap_stream() };
+
 	return 0;
 }
